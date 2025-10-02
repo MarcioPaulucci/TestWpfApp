265431b323afe87948892172700ff03d431611d0
Marcio <maptech80@gmail.com>
2025-10-02 18:31:52 -0300

    Implementada uma ToDo List.

--- TestWpfApp/MainWindow.xaml.cs
+++ TestWpfApp/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TestWpfApp
 {
@@ -16,9 +8,36 @@ namespace TestWpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<TodoItem> Tasks { get; set; } = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            TodoList.ItemsSource = Tasks;
         }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            var desc = TaskInput.Text.Trim();
+            if (!string.IsNullOrEmpty(desc))
+            {
+                Tasks.Add(new TodoItem { Description = desc });
+                TaskInput.Text = "";
+            }
+        }
+
+        private void RemoveTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement fe && fe.Tag is TodoItem item)
+            {
+                Tasks.Remove(item);
+            }
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
     }
 }
